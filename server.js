51004ae6422f3a2673347b372c39c3af677a268f
Christopher WJ Rueber <crueber@gmail.com>
2015-02-06 10:29:00 -0600

    Add argv pulls for ports.

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 
-var http, port, hostname, redirector, server, location;
+var http, hostname, redirector, server, from_port, to_port;
 
 http = require('http');
 
-port = process.env.PORT || 81;
+from_port = process.argv[2] || 80;
+to_port = process.argv[3] || 443;
 
 redirector = function (req, res) {
-  location = "https://" + req.headers['host'] + req.originalUrl;
+  var host, location;
+  host = req.headers['host'].split(':')[0];
+  if (to_port != 443) { host = host + ":" + to_port; }
+  location = "https://" + host + req.url;
   res.writeHead(301, { 'Location': location });
   res.end();
-  console.log((new Date()).toJSON() + ': Redirecting ' + req.ip + ' from http to https on path ' + req.url + '.');
+  console.log((new Date()).toJSON() + ': Redirecting ' + req.ip + ' from http('+from_port+') to https('+to_port+') on path ' + req.url + '.');
 }
 server = http.createServer(redirector);
 
 if (hostname = process.env.HOSTNAME) {
-  server.listen(port, hostname);
+  server.listen(from_port, hostname);
 }
 else {
-  server.listen(port);
+  server.listen(from_port);
 }
 
-console.log('Redirector listening on port '+port+'.');
+console.log('Redirector listening on port '+from_port+'.');
 
